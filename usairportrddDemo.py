631a86a3a0f9eddea84c02a191e32d4f658b92bc
amit upadhayay <akupadhayay@gmail.com>
2018-09-14 23:54:36 +0530

    adding rdd demo

---
+++ usairportrddDemo.py
@@ -0,0 +1,22 @@
+from pyspark import SparkContext, SparkConf
+import sys
+
+def genOutput(line):
+    splits = line.split(",")
+    return "{}, {}".format(splits[1], splits[2])
+
+if __name__ == "__main__":
+    if len(sys.argv)==3:
+        conf = SparkConf().setAppName("US airports rdd demo app").setMaster("local[*]")
+        sc = SparkContext(conf = conf)
+        in_path=sys.argv[1]
+        airportsrdd = sc.textFile(in_path)
+
+        airportsusarddfltrd = airportsrdd.filter(lambda line: line.split(",")[3]=="\"United States\"")
+        airportcitynamesedd =  airportsusarddfltrd.map(lambda line:genOutput(line))
+        out_path=sys.argv[2]
+        airportcitynamesedd.saveAsTextFile(out_path)
+
+    else:
+        print("Invalid number of arguments passed")
+        exit(1)
